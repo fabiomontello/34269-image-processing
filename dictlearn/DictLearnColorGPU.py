2c0687e83aa0f0b03a9481c6ee64d28bfc59e7f3
Zach M <zachary1131@gmail.com>
2024-07-22 16:25:25 +0200

    Y=grey

--- dictlearn/DictLearnColorGPU.py
+++ dictlearn/DictLearnColorGPU.py
@@ -133,14 +133,14 @@ def colorizeImg(greyImg,dictY, dictCb, dictCr,patchSze,mxPatches,numComp):
     print(recPatchCb.shape)
 
     # return to original shape
-    recPatchY = greyImg#recPatchY.reshape(patchesY.shape)
+    recPatchY = recPatchY.reshape(patchesY.shape)
     recPatchCb = recPatchCb.reshape(patchesCb.shape)
     recPatchCr = recPatchCr.reshape(patchesCr.shape)
     print(recPatchCb.shape)
     print(recPatchCb)
 
     # reconstruct channels from patches
-    recY = reconstruct_from_patches_2d(recPatchY, greyImg.shape)
+    recY = greyImg#reconstruct_from_patches_2d(recPatchY, greyImg.shape)
     recCb = reconstruct_from_patches_2d(recPatchCb, greyImg.shape)
     recCr = reconstruct_from_patches_2d(recPatchCr, greyImg.shape)
     print('recCb')
