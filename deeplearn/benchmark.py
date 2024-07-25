a4bd8cab700b33f891f13836cc34a252bc9e0e5e
Carlos Prieto <carlos2000.prieto@gmail.com>
2024-07-25 14:08:18 +0200

    Update benchmark.py

--- deeplearn/benchmark.py
+++ deeplearn/benchmark.py
@@ -1,69 +1,69 @@
-import colour
 import numpy as np
 import os
 import imageio.v3 as iio
-from skimage import color
+import cv2
 from sklearn.metrics import root_mean_squared_error
-from sktime.performance_metrics.forecasting import mean_relative_absolute_error
 
 from transforms import TransformsFinetune
-#from PIL import Image
-#import matplotlib.pyplot as plt
 
 
 def benchmark(image, imageTrue, wantDeltaE=True):
-    #Split images into RGB
+
     R1, G1, B1 = image[:, :, 0], image[:, :, 1], image[:, :, 2]
     R2, G2, B2 = imageTrue[:, :, 0], imageTrue[:, :, 1], imageTrue[:, :, 2]
-
-    #Calculate rmse for each channel
+    
     rmse_R = root_mean_squared_error(R1, R2)
     rmse_G = root_mean_squared_error(G1, G2)
     rmse_B = root_mean_squared_error(B1, B2)
-    rmse_total = (rmse_R + rmse_G + rmse_B) / 3
-
-    #Calculate mrae for each channel
-    #Need a benchmark image for MRAE, using the ground image as benchmark leads to essentially dividing by 0
-    mrae_R = mean_relative_absolute_error(R2, R1, y_pred_benchmark=R2) 
-    mrae_G = mean_relative_absolute_error(G2, G1, y_pred_benchmark=G2)
-    mrae_B = mean_relative_absolute_error(B2, B1, y_pred_benchmark=B2)
-    mrae_total = (mrae_R + mrae_G + mrae_B) / 3
-
     
-    if wantDeltaE == True: #Can run function with only the two fast metric
-        imageLAB = color.rgb2lab(image)
-        imageTrueLAB = color.rgb2lab(imageTrue)
-        delta_E_pixels = np.zeros([imageLAB.shape[0], imageLAB.shape[1]])
-        #Calculate delta_E for each pixel in the images, 
-        for i in range(imageLAB.shape[0]):
-            for j in range(imageLAB.shape[1]):
-                delta_E_pixels[i,j] = colour.delta_E(imageLAB[i,j,:], imageTrueLAB[i,j,:], method="CIE 2000")
-        #Average of all delta_E values
-        delta_E = np.mean(delta_E_pixels)
+    rmse_total = (rmse_R + rmse_G + rmse_B) / 3    
+    delta_E = deltaE(image, imageTrue)
+
+    return rmse_total, delta_E
+
+#The deltaE function was taken from the last comment in: https://stackoverflow.com/questions/57224007/how-to-compute-the-delta-e-between-two-images-using-opencv
+def deltaE(img1, img2, colorspace = cv2.COLOR_RGB2LAB):
+    # check the two images are of the same size, else resize the two images
+    (h1, w1) = img1.shape[:2]
+    (h2, w2) = img1.shape[:2]
+    h, w = None, None
+    # check the height
+    if h1 > h2:
+        h = h1
     else:
-        delta_E = None
+        h = h2
+    #check the width
+    if w1 > w2:
+        w = w1
+    else:
+        w = w2
     
-    return rmse_total, mrae_total, delta_E
+    img1 = cv2.resize(img1, (h,w))
+    img2 = cv2.resize(img2, (h,w))
+    # Convert BGR images to specified colorspace
+    img1 = cv2.cvtColor(img1, colorspace)
+    img2 = cv2.cvtColor(img2, colorspace)
+    # compute the Euclidean distance with pixels of two images 
+    return np.mean(np.sqrt(np.sum((img1 - img2) ** 2, axis=-1)))
 
 def getScores(getDelta=True, filepath="output/"):
-    #Transform turns test folder images to same resolution as rgb folder
     transform = TransformsFinetune(train=False, image_size=224)
     files = os.listdir(filepath + "test")
-    scores = np.zeros([len(files), 3])
+    scores = np.zeros([len(files),2])
     i = 0
     
     for file in files:
-        if not file.startswith('.'): #Ignore .DS_Store file that is sometimes in folders
-            #Takes each file in test and finds the 
-            rgb = iio.imread(filepath + "rgb/" + file, pilmode="RGB")
+        if not file.startswith('.'):# and os.path.isfile(os.path.join(root, file)):
+            rgb = iio.imread(filepath + "ycbcr_two_norm/" + file, pilmode="RGB")
             test = iio.imread(filepath + "test/" + file, pilmode="RGB")
             tr_test = transform(test).numpy()
             tr_test = tr_test.transpose(1, 2, 0)
-            #With the two images loaded and test in the right form, gets benchmarks. Passes whether to get the delta_E or not to the benchmark function
-            scores[i] = benchmark(rgb, tr_test, getDelta)
+            print(file, "rgb:", rgb.shape, "test:", test.shape, "tr_test", tr_test.shape)
+            scores[i] = benchmark(rgb, tr_test)
+            print(i, file, scores[i])
             i+=1
-    scores = scores[~(scores == 0).all(axis=1)] #Deletes rows with 0s if some files were ignored
+    scores = scores[~(scores == 0).all(axis=1)]
     return scores
 
-#Runs the function once, goes through both folders
 scores = getScores()
+scoresAvg = np.mean(scores, axis=0)
